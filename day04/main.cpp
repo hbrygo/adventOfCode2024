57cb9b00a9beed0c9e7460cca739e327282d1d02
Hugo Brygo <hubrygo@fu-c3-p4.s19.be>
2024-12-16 16:51:03 +0100

    day04 fini

--- day04/main.cpp
+++ day04/main.cpp
@@ -3,18 +3,45 @@
 #include <regex>
 #include <string>
 
-int main() {
-    std::string filename = "input.txt"; // Nom du fichier à lire
-    std::ifstream file(filename);
+static bool check(int x, int y, std::vector<std::string> &tab, int dx, int dy)
+{
+	const std::string xmas = "MAS";
+	for (int i = 0; i < 3; ++i)
+		if (tab[y + i * dy][x + i * dx] != xmas[i])
+			return 0;
+	return 1;
+}
 
-    if (!file.is_open()) {
-        std::cerr << "Erreur : Impossible d'ouvrir le fichier." << std::endl;
-        return 1;
+int main()
+{
+	std::string filename = "input.txt"; // Nom du fichier à lire
+	std::ifstream file(filename);
+
+	if (!file.is_open())
+	{
+		std::cerr << "Erreur : Impossible d'ouvrir le fichier." << std::endl;
+		return 1;
 	}
-	std::string fullFile;
 	std::string line;
-	while (std::getline(file, line)) {
-		fullFile += line + "\n";
+	std::vector<std::string> tab;
+	while (std::getline(file, line))
+		tab.push_back(line);
+
+	int res = 0;
+	for (int i = 1; i < tab.size() - 1; ++i)
+	{
+		for (int j = 1; j < tab[i].size() - 1; ++j)
+		{
+			if (tab[i][j] == 'A')
+			{
+				int count = 0;
+				count += check(j + 1, i + 1, tab, -1, -1);
+				count += check(j - 1, i - 1, tab, 1, 1);
+				count += check(j + 1, i - 1, tab, -1, 1);
+				count += check(j - 1, i + 1, tab, 1, -1);
+				res += count == 2;
+			}
+		}
 	}
-	printf("file:\n%s", fullFile.c_str());
+	std::cout << res << std::endl;
 }
